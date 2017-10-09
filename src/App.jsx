fb328d4561e11d4d7010f9fcd35e8364bbaad899
Fernando Moreira <nandomoreira.me@gmail.com>
2017-10-09 19:20:53 -0300

    eslint and deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import logo from './images/logo.svg';
+import './css/app.css';
 
 class App extends Component {
   render() {
